b5777dd9615653e7d35dfd5ba7c4aed91a2ce6ab
TheEmuDev <thatemudev@gmail.com>
2020-03-29 21:16:31 -0500

    react-router-dom added
    Added so I can better work on chart in own 'page'

---
+++ spotify-dashboard/client/src/components/Chart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Polar } from 'react-chartjs-2';
+
+class Chart extends Component {
+    render() {
+        return (
+            <div className="chart">
+                CHART COMPONENT
+            </div>
+        );
+    }
+}
+
+export default Chart;
