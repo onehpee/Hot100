b61ba55c2fb12c8ce17e782d039003de9c86d448
TheEmuDev <thatemudev@gmail.com>
2020-04-07 18:17:09 -0500

    mini commit
    changing details in the graph

--- spotify-dashboard/client/src/components/Chart.js
+++ spotify-dashboard/client/src/components/Chart.js
@@ -12,11 +12,18 @@ class Chart extends Component {
                 14
             ],
             backgroundColor: [
+                '#FF6384c0',
+                '#4BC0C0c0',
+                '#FFCE56c0',
+                '#E7E9EDc0',
+                '#36A2EBc0'
+            ],
+            borderColor:[
                 '#FF6384',
                 '#4BC0C0',
                 '#FFCE56',
                 '#E7E9ED',
-                '#36A2EB'
+                '#36A2EB',
             ],
             label: 'My Dataset' // for legend
         }],
