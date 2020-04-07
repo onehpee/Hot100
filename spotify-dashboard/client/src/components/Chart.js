358f52ad8970655dc7a66d1aa6f508ab9440eb2f
TheEmuDev <thatemudev@gmail.com>
2020-04-07 10:15:39 -0500

    Update Chart.js
    Now shows a chart on polar-chart page

--- spotify-dashboard/client/src/components/Chart.js
+++ spotify-dashboard/client/src/components/Chart.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react'
 import { Polar } from 'react-chartjs-2';
 
 class Chart extends Component {
+    state = {
+        datasets: [{
+            data: [
+                11,
+                16,
+                7,
+                3,
+                14
+            ],
+            backgroundColor: [
+                '#FF6384',
+                '#4BC0C0',
+                '#FFCE56',
+                '#E7E9ED',
+                '#36A2EB'
+            ],
+            label: 'My Dataset' // for legend
+        }],
+        labels: [
+            'Red',
+            'Green',
+            'Yellow',
+            'Grey',
+            'Blue'
+        ]
+    };
+
     render() {
         return (
             <div className="chart">
-                CHART COMPONENT
+                <Polar data={this.state} />
             </div>
         );
     }
