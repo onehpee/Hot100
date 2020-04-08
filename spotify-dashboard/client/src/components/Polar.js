dea771d5f45767b10ab4a0cbe97252740aa70360
TheEmuDev <thatemudev@gmail.com>
2020-04-08 13:52:56 -0500

    Update Polar.js
    moved chart to polar

--- spotify-dashboard/client/src/components/Polar.js
+++ spotify-dashboard/client/src/components/Polar.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Polar } from 'react-chartjs-2';
 
+class Chart extends Component {
+    state = {
+        datasets: [{
+            data: [
+                11,
+                16,
+                7,
+                3,
+                14
+            ],
+            backgroundColor: [
+                '#FF6384c0',
+                '#4BC0C0c0',
+                '#FFCE56c0',
+                '#E7E9EDc0',
+                '#36A2EBc0'
+            ],
+            borderColor:[
+                '#FF6384',
+                '#4BC0C0',
+                '#FFCE56',
+                '#E7E9ED',
+                '#36A2EB',
+            ],
+            label: 'My Dataset' // for legend
+        }],
+        labels: [
+            'Red',
+            'Green',
+            'Yellow',
+            'Grey',
+            'Blue'
+        ]
+    };
 
-
-function Polar() {
-  return (
-    <div className="App">
-    <h1> Polar </h1>
-      
-      </div>
-    
-  )
+    render() {
+        return (
+            <div className="chart">
+                <Polar data={this.state} />
+            </div>
+        );
+    }
 }
 
-export default Polar
+export default Chart;
