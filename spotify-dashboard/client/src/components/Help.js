ce820fb9b701a5a4f71311ac9bf7469754134a76
manan270 <mananamin@discover.com>
2020-04-12 15:13:07 -0500

    help page added

--- spotify-dashboard/client/src/components/Help.js
+++ spotify-dashboard/client/src/components/Help.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import '../App.css';
 
-
-
-function Help() {
-  return (
-    <div className="App">
-    <h1> Help </h1>
-      
+export default class Help extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <h1> Contact info:</h1>
+        <table className="apps" align="center" cellpadding="5" border="5">
+          <tr>
+              <th>Name</th>
+              <th>Contact</th>
+          </tr>
+          <tr>
+              <td>Michael Kucharski</td>
+              <td>michaelkucharski@discover.com</td>
+          </tr>
+          <tr>
+              <td>Manan Amin</td>
+              <td>mananamin@discover.com</td>
+          </tr>
+          <tr>
+              <td>Dean LaBarbera</td>
+              <td>deanlabarbera@discover.com</td>
+          </tr>
+          <tr>
+              <td>Crystal Ritchey</td>
+              <td>crystalritchey@discover.com</td>
+          </tr>
+          <tr>
+              <td>Syed Rizvi</td>
+              <td>syedrizvi@discover.com</td>
+          </tr>
+          <tr>
+              <td>Adedayo Uwensuyi</td>
+              <td>adedayouwensuyi@discover.com</td>
+          </tr>
+          <tr>
+              <td>Destinee Clinhscales</td>
+              <td>destineeclinhscales@discover.com</td>
+          </tr>
+        </table>
       </div>
-    
-  )
-}
+      
+    )
+} }
 
-export default Help
