e0188bc30651a65f4c8b986370452d4927e17372
manan270 <mananamin@discover.com>
2020-04-21 01:35:01 -0500

    chart added

--- spotify-dashboard/client/src/components/Chart.js
+++ spotify-dashboard/client/src/components/Chart.js
@@ -6,16 +6,16 @@ export default class Chart extends Component {
   
   render() {
     const data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: ['Blinding Lights', 'Toosie Slide', 'Roses', 'death bed', 'Dance Monkey'],
       datasets: [
         {
-          label: 'My First dataset',
-          backgroundColor: 'rgba(255,99,132,0.2)',
-          borderColor: 'rgba(255,99,132,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [65, 59, 80, 81, 56, 55, 40]
+          label: 'Top 5 songs on Spotify',
+          backgroundColor: 'rgba(178, 5, 8, 0.5)',
+          borderColor: 'rgb(0, 0, 0)',
+          borderWidth: 2,
+          hoverBackgroundColor: 'rgb(214, 95, 10)',
+          hoverBorderColor: 'rgb(10, 214, 193)',
+          data: [5585890, 5117365, 4443330, 3969465, 3584848]
         }
       ]
     };
